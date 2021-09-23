85a90fac9f21b3f20fcec497ce32077031f7ad5d
Bentt0201 <bem0507@hotmail.com>
2021-09-23 21:55:04 +0800

    add Expenses Compoents

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+// components
+import Expenses from './components/Expenses';
+
 function App() {
+  const expenses = [
+    {
+      id: 'el1',
+      title: 'lesson1',
+      amount: 300,
+      date: new Date(2021, 9, 1),
+    },
+    {
+      id: 'el2',
+      title: 'lesson2',
+      amount: 700,
+      date: new Date(2021, 9, 12),
+    },
+    {
+      id: 'el3',
+      title: 'lesson3',
+      amount: 500,
+      date: new Date(2021, 9, 21),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>Test</p>
+      <Expenses item={expenses} />
     </div>
   );
 }
